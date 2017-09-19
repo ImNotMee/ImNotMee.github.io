10d17a315c70b3e8bfb1dae63a34b25090f89b82
Veracities <philip200156@hotmail.com>
2017-09-19 10:50:47 -0400

    Added hobbies on "main.html"
    Added comments on "loadingScreen.js"

--- JavaScript/loadingScreen.js
+++ JavaScript/loadingScreen.js
@@ -1,15 +1,17 @@
 (function() {
-    
-    var preload = document.getElementById("preload");
+    // the variable to keep track of how long the loading screen stays for
     var loading = 0;
     var id = setInterval(frame, 64);
     
     function frame() {
+        // Load the main.html when it reach the time
         if (loading == 30) {
             clearInterval(id);
             window.location.replace("main.html");
         } else {
+            // Add one to loading
             loading = loading +1;
+            // Cast the fade animation when it reaches the end of the loading time
             if (loading == 20) {
                 preload.style.animation = "fadeout 1s ease";
             }
